25cbd54c52d2f422242cf83a8664e58dfc490008
Muntazir0972 <mohammadmuntazir18@gmail.com>
2024-08-01 18:43:36 +0500

    apply middleware,21

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckAdmin;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+
+        $middleware->alias([
+            'check-admin' => CheckAdmin::class
+        ]);    
+
         $middleware->redirectTo(
           guests:'account/login',
           users:'account/profile'
